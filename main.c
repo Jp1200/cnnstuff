536f24276f9c01db1727e3e938d7c9b53ee57c5b
Jack Parker <jprkr96@gmail.com>
2025-08-20 09:32:47 -0500

    first compile with all steps

--- main.c
+++ main.c
@@ -74,19 +74,94 @@ void flatten(float input[(IMG_SIZE-2)/POOL_SIZE][(IMG_SIZE-2)/POOL_SIZE], float 
   }
 }
 //Forward pass for fully connected layers
-void fc_forward(float input[((IMG_SIZE-2)/POOL_SIZE)*((IMG_SIZE-2)/POOL_SIZE)], float output[FC_SIZE],float weights[FC_SIZE][((IMG_SIZE-2)/POOL_SIZE)*((IMG_SIZE-2)/POOL_SIZE)], float bias[FC_SIZE]){
-  for (int i = 0; i < FC_SIZE; i++) {
-    float sum = bias[i];
-    for (int j = 0;j < ((IMG_SIZE-2)/POOL_SIZE)*((IMG_SIZE-2)/POOL_SIZE);j++) {
-      sum += weights[i][j]*input[j];
+void fc_forward(float input[((IMG_SIZE-2)/POOL_SIZE)*((IMG_SIZE-2)/POOL_SIZE)], float weights[FC_SIZE][((IMG_SIZE-2)/POOL_SIZE)*((IMG_SIZE-2)/POOL_SIZE)], float bias[FC_SIZE], float output[FC_SIZE]) {
+    for(int i = 0; i < FC_SIZE; i++) {
+        float sum = bias[i];
+        for(int j = 0; j < ((IMG_SIZE-2)/POOL_SIZE)*((IMG_SIZE-2)/POOL_SIZE); j++){
+            sum += weights[i][j] * input[j];
+        }
+        output[i] = relu(sum);
     }
-    output[i] = relu(sum);
-  }
 }
 int main(){
+  clock_t start_time, end_time;
+  start_time = clock();
   srand(time(0));
+  float image[IMG_SIZE][IMG_SIZE];
+  for (int i = 0; i < IMG_SIZE; i++) {
+    for (int j = 0; j < IMG_SIZE; j++) {
+      image[i][j] = rand_float();
+      //todo for training downsize images to 100x100 pixel grayscale
+      //instead of random float images
+    }
+  }
+  //convolution layers
+  float filters[NUM_FILTERS][FILTER_SIZE][FILTER_SIZE];
+  for (int f = 0; f < NUM_FILTERS; f++) {
+    for (int i = 0; i < FILTER_SIZE; i++) {
+      for (int j = 0; j < FILTER_SIZE; j++) {
+        filters[f][i][j] = rand_float();
+      }
+    }
+  }
+  float conv_output[NUM_FILTERS][IMG_SIZE-2][IMG_SIZE-2];
+  for (int f = 0; f < NUM_FILTERS; f++) {
+    conv2d(image, conv_output[f],filters[f]);
+  }
+  //pooling 
+  float pool_out[NUM_FILTERS][(IMG_SIZE-2)/POOL_SIZE][(IMG_SIZE-2)/POOL_SIZE];
+  for (int f = 0; f < NUM_FILTERS; f++) {
+    maxpool2d(conv_output[f],pool_out[f]);
+  }
+  //flatten and combine
+  float flattened[NUM_FILTERS*((IMG_SIZE-2)/POOL_SIZE)*((IMG_SIZE-2)/POOL_SIZE)];
+  int idx = 0;
+  for (int f = 0; f < NUM_FILTERS; f++) {
+    for (int i = 0; i < (IMG_SIZE-2)/POOL_SIZE; i++) {
+      for (int j = 0; j < (IMG_SIZE-2)/POOL_SIZE; j++) {
+        flattened[idx++] = pool_out[f][i][j];
+      }
+    }
+  }
 
+ 
+ float fc_weights[FC_SIZE][((IMG_SIZE-2)/POOL_SIZE)*((IMG_SIZE-2)/POOL_SIZE)];
+    float fc_bias[FC_SIZE];
+    for(int i = 0; i < FC_SIZE; i++) {
+        fc_bias[i] = rand_float();
+        for(int j = 0; j < ((IMG_SIZE-2)/POOL_SIZE)*((IMG_SIZE-2)/POOL_SIZE); j++)
+            fc_weights[i][j] = rand_float();
+    }
+  float fc_out[FC_SIZE];
+  fc_forward(flattened, fc_weights, fc_bias, fc_out);
+// Output layer (softmax)
+  float output[NUM_CLASSES] = {0};
+  float out_weights[NUM_CLASSES][FC_SIZE];
+  float out_bias[NUM_CLASSES];
+  for(int i = 0; i < NUM_CLASSES; i++) {
+    out_bias[i] = rand_float();
+    for(int j = 0; j < FC_SIZE; j++)
+        out_weights[i][j] = rand_float();
+  }
 
+  // Compute final logits
+  float logits[NUM_CLASSES];
+  for(int i = 0; i < NUM_CLASSES; i++) {
+    logits[i] = out_bias[i];
+    for(int j = 0; j < FC_SIZE; j++){
+      logits[i] += out_weights[i][j] * fc_out[j];
+    }
+  }
+
+  // Softmax probabilities
+  softmax(logits, output);
+
+  printf("Output probabilities:\n");
+  for(int i = 0; i < NUM_CLASSES; i++)
+        printf("Class %d: %.4f\n", i, output[i]);
+  end_time = clock();
+  double time_taken = (double)(end_time - start_time) / CLOCKS_PER_SEC;
+  printf("Time in Seconds to execute: %f\n", time_taken);
   return 0;
 }
 
